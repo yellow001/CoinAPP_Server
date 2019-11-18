1540abdd6296cebb52d44202b8b814d77b57ab75
yellowzhong001 <1144916875@qq.com>
2019-11-18 16:53:11 +0800

    基本框架

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using NetFrame.Base;
+using NetFrame.EnDecode;
+using NetFrame.EnDecode.Extend;
+using NetFrame.Interfaces;
+using NetFrame.Tool;
+using System;
+
+namespace CoinAPP_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BaseServer<TransModel, H5Token> server = new BaseServer<TransModel, H5Token>(12345);
+            //AbsCoding.Ins = new PbCoding();
+            //server.Init(IMessageHandler.Ins);
+            //server.Start();
+
+            //DbHelperMySQL.connectionString = AppSetting.Ins.Settings["connstring"];
+
+            //CidModel m = new CidModel(1, 11, 111, -500);
+            //byte[] ms = SeProtobuf.Serialization(m);
+
+            //CidModel m2 = SeProtobuf.DeSerialization<CidModel>(ms);
+
+            //Console.WriteLine(m2.cid);
+
+            //Console.WriteLine(AppSetting.GetValue("test"));
+            
+
+            //DbHelperMySQL.connectionString = "server=120.25.84.142;database=TaskServer;uid=yellow;pwd=qW789456123=";
+
+            //DataResult dr = DbHelperMySQL.Query("select * from user");
+
+            //Console.WriteLine(dr.rows);
+
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}

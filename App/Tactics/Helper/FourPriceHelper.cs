e09d6dd7f1188730c609e3845a9c1464aa104210
yellowzhong <1144916875@qq.com>
2020-10-06 17:57:06 +0800

    修改策略

--- App/Tactics/Helper/FourPriceHelper.cs
+++ App/Tactics/Helper/FourPriceHelper.cs
@@ -9,6 +9,11 @@ using System.Threading.Tasks;
 
 public class FourPriceHelper : BaseTaticsHelper
 {
+    KLine m_LastKLine;
+    KLine m_CurKLine;
+
+    float MergeHour = 4;
+
     #region 重载
 
     /// <summary>
@@ -18,11 +23,12 @@ public class FourPriceHelper : BaseTaticsHelper
     public override void Init(string setting)
     {
         string[] strs = setting.Split(';');
-        if (strs.Length >= 3)
+        if (strs.Length >= 4)
         {
             V_Instrument_id = strs[0];
-            V_Min = int.Parse(strs[1]);
-            V_Leverage = float.Parse(strs[2]);
+            MergeHour = float.Parse(strs[1]);
+            V_Min = int.Parse(strs[2]);
+            V_Leverage = float.Parse(strs[3]);
         }
         //Console.WriteLine(V_Instrument_id + ":合约 " + V_Instrument_id);
         base.Init(setting);
@@ -43,6 +49,11 @@ public class FourPriceHelper : BaseTaticsHelper
         Debugger.Warn(V_Instrument_id + ":分析历史数据完毕");
     }
 
+    public override void ClearRunData()
+    {
+        m_CurKLine = null;
+    }
+
     /// <summary>
     /// 下单
     /// </summary>
@@ -51,6 +62,14 @@ public class FourPriceHelper : BaseTaticsHelper
     /// </returns>
     public override int MakeOrder(bool isTest=false)
     {
+        //Task.Run(async () => { await F_GetDayKLine(); }).Wait();
+        GetMergeKLine(V_Cache.V_KLineData[0].V_Timestamp, ref m_CurKLine, ref m_LastKLine);
+
+        if (m_CurKLine == null || m_LastKLine == null)
+        {
+            return 0;
+        }
+
         return GetValue(true, 0,isTest);
     }
 
@@ -62,6 +81,15 @@ public class FourPriceHelper : BaseTaticsHelper
     /// <returns></returns>
     protected override bool OnShouldCloseOrder(int dir, float percent, bool isTest = false)
     {
+        //Task.Run(async () => { await F_GetDayKLine(); }).Wait();
+
+        GetMergeKLine(V_Cache.V_KLineData[0].V_Timestamp, ref m_CurKLine, ref m_LastKLine);
+
+
+        if (m_CurKLine == null || m_LastKLine == null) {
+            return false;
+        }
+
         if (percent <= lossPercent)
         {
             //无条件止损
@@ -69,29 +97,46 @@ public class FourPriceHelper : BaseTaticsHelper
         }
         else
         {
-            //int result = GetMAPriceResult(dir);
+            int result = GetMAPriceResult(dir);
 
-            //if (percent <= lossPercent * 0.1f && result > 0)
+            if (percent <= lossPercent * 0.1f && result > 0)
+            {
+                long time = DateTime.UtcNow.Ticks - V_LastOpTime.Ticks;
+                if (isTest)
+                {
+                    time = V_Cache.V_KLineData[0].V_Timestamp.Ticks - V_LastOpTime.Ticks;
+                }
+
+                bool shouldReset = time - V_Min * Util.Minute_Ticks <= 0;
+
+                return shouldReset;
+            }
+
+            //if (percent <= lossPercent * 0.5f && result > 0)
             //{
-            //    long time = DateTime.UtcNow.Ticks - V_LastOpTime.Ticks;
-            //    if (isTest)
-            //    {
-            //        time = V_Cache.V_KLineData[0].V_Timestamp.Ticks - V_LastOpTime.Ticks;
-            //    }
-
-            //    bool shouldReset = time - V_Min * Util.Minute_Ticks <= 0;
-
-            //    return shouldReset;
+            //    return true;
             //}
 
 
-            int sign = GetValue(false, dir, isTest);
+            int sign = GetValue(false, dir);
 
             if (percent >= winPercent)
             {
                 maxAlready = true;
                 return sign > 0;
             }
+
+            //if (percent < lossPercent * 0.5f && sign > 0)
+            //{
+            //    //指标反向+亏损，溜吧
+            //    return true;
+            //}
+
+            //if (maxAlready && sign > 0)
+            //{
+            //    //如果曾经到达过最高而指标反向，止盈一下吧
+            //    return true;
+            //}
 
             DateTime t = DateTime.UtcNow;
 
@@ -114,6 +159,82 @@ public class FourPriceHelper : BaseTaticsHelper
         return MA.GetMA(length, V_Cache.V_KLineData);
     }
 
+    void GetMergeKLine(DateTime curDataTime,ref KLine curLine,ref KLine lastLine) {
+
+        bool refresh = false;
+
+        DateTime time = new DateTime(curDataTime.Year, curDataTime.Month, curDataTime.Day, curDataTime.Hour, 0, 0);
+
+        if (m_LastKLine == null || m_CurKLine == null)
+        {
+            refresh = true;
+        }
+        else
+        {
+            if (m_CurKLine.V_Timestamp.Ticks < time.Ticks)
+            {
+                refresh = true;
+            }
+        }
+
+        if (refresh) {
+            curLine = new KLine();
+            lastLine = new KLine();
+
+            DateTime lastDataTime = time.AddHours(-MergeHour);
+            DateTime nextDataTime = time.AddHours(MergeHour);
+
+            curLine.V_Timestamp = time;
+            lastLine.V_Timestamp = lastDataTime;
+
+            List<KLine> curList = new List<KLine>();
+            List<KLine> lastList = new List<KLine>();
+            for (int i = 0; i < V_Cache.V_KLineData.Count; i++)
+            {
+                KLine data = V_Cache.V_KLineData[i];
+                if (data.V_Timestamp <= nextDataTime && data.V_Timestamp >= time)
+                {
+                    curList.Add(data);
+                }
+                else if (data.V_Timestamp <= time && data.V_Timestamp >= lastDataTime)
+                {
+                    lastList.Add(data);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            for (int i = 0; i < curList.Count; i++)
+            {
+                if (curList[i].V_Timestamp == curDataTime)
+                {
+                    curLine.V_OpenPrice = curList[i].V_OpenPrice;
+                }
+            }
+
+            for (int i = 0; i < lastList.Count; i++)
+            {
+                if (lastLine.V_HightPrice == 0)
+                {
+                    lastLine.V_HightPrice = lastList[i].V_HightPrice;
+                    lastLine.V_LowPrice = lastList[i].V_LowPrice;
+                }
+
+                if (lastList[i].V_LowPrice < lastLine.V_LowPrice)
+                {
+                    lastLine.V_LowPrice = lastList[i].V_LowPrice;
+                }
+
+                if (lastList[i].V_HightPrice > lastLine.V_HightPrice)
+                {
+                    lastLine.V_HightPrice = lastList[i].V_HightPrice;
+                }
+            }
+        }
+    }
+
     #endregion
 
 
@@ -122,47 +243,86 @@ public class FourPriceHelper : BaseTaticsHelper
 
     int GetValue(bool isOrder, int orderDir,bool isTest=false)
     {
-        if (!isTest)
-        {
-            DateTime t = DateTime.UtcNow;
-            if ((V_Min - t.Minute % V_Min) != 1)
-            {
-                return 0;
-            }
-        }
-
         if (isOrder)
         {
-            if (V_Cache.V_KLineData[0].V_HightPrice >= V_Cache.V_KLineData[1].V_HightPrice)
+
+            if (isTest)
             {
-                //if (V_Cache.V_KLineData[0].V_HightPrice >= MA60)
-                //{
-                return 1;
-                //}
+                if (V_Cache.V_KLineData[0].V_HightPrice >= m_LastKLine.V_HightPrice)
+                {
+                    //if (V_Cache.V_KLineData[0].V_HightPrice >= MA60)
+                    //{
+                        return 1;
+                    //}
+                }
+                else if (V_Cache.V_KLineData[0].V_LowPrice <= m_LastKLine.V_LowPrice)
+                {
+                    //if (V_Cache.V_KLineData[0].V_LowPrice <= MA60)
+                    //{
+                        return -1;
+                    //}
+                }
             }
-            else if (V_Cache.V_KLineData[0].V_LowPrice <= V_Cache.V_KLineData[1].V_LowPrice)
-            {
-                //if (V_Cache.V_KLineData[0].V_LowPrice <= MA60)
-                //{
-                return -1;
-                //}
+            else {
+
+                DateTime t = DateTime.UtcNow;
+                if ((V_Min - t.Minute % V_Min) != 1) {
+                    return 0;
+                }
+
+                if (V_Cache.V_KLineData[0].V_ClosePrice >= m_LastKLine.V_HightPrice)
+                {
+                    //if (V_Cache.V_KLineData[0].V_ClosePrice >= MA60)
+                    //{
+                        return 1;
+                    //}
+                }
+                else if (V_Cache.V_KLineData[0].V_ClosePrice <= m_LastKLine.V_LowPrice)
+                {
+                    //if (V_Cache.V_KLineData[0].V_ClosePrice <= MA60)
+                    //{
+                        return -1;
+                    //}
+                }
             }
         }
         else {
-            if (orderDir > 0)
+
+            if (isTest)
             {
-                //跌破开盘 止损
-                if (V_Cache.V_KLineData[0].V_ClosePrice <= V_Cache.V_KLineData[0].V_OpenPrice)
+                if (orderDir > 0)
                 {
-                    return 1;
+                    //跌破开盘 止损
+                    if (V_Cache.V_KLineData[0].V_LowPrice <= m_CurKLine.V_OpenPrice)
+                    {
+                        return 1;
+                    }
+                }
+                else if (orderDir < 0)
+                {
+                    //涨破开盘 止损
+                    if (V_Cache.V_KLineData[0].V_HightPrice >= m_CurKLine.V_OpenPrice)
+                    {
+                        return 1;
+                    }
                 }
             }
-            else if (orderDir < 0)
-            {
-                //涨破开盘 止损
-                if (V_Cache.V_KLineData[0].V_ClosePrice >= V_Cache.V_KLineData[0].V_OpenPrice)
+            else {
+                if (orderDir > 0)
                 {
-                    return 1;
+                    //跌破开盘 止损
+                    if (V_Cache.V_KLineData[0].V_ClosePrice <= m_CurKLine.V_OpenPrice)
+                    {
+                        return 1;
+                    }
+                }
+                else if (orderDir < 0)
+                {
+                    //涨破开盘 止损
+                    if (V_Cache.V_KLineData[0].V_ClosePrice >= m_CurKLine.V_OpenPrice)
+                    {
+                        return 1;
+                    }
                 }
             }
         }
@@ -173,20 +333,42 @@ public class FourPriceHelper : BaseTaticsHelper
     public int GetMAPriceResult(int orderDir, bool isTest = false) {
         float MAPrice = F_GetMA(20);
 
-        if (orderDir > 0)
+        if (isTest)
         {
-            //跌破均线 考虑走人
-            if (V_Cache.V_KLineData[0].V_ClosePrice <= MAPrice)
+            if (orderDir > 0)
             {
-                return 1;
+                //跌破均线 考虑走人
+                if (V_Cache.V_KLineData[0].V_LowPrice <= MAPrice)
+                {
+                    return 1;
+                }
+            }
+            else if (orderDir < 0)
+            {
+                //涨破均线 考虑走人
+                if (V_Cache.V_KLineData[0].V_HightPrice >= MAPrice)
+                {
+                    return 1;
+                }
             }
         }
-        else if (orderDir < 0)
+        else
         {
-            //涨破均线 考虑走人
-            if (V_Cache.V_KLineData[0].V_ClosePrice >= MAPrice)
+            if (orderDir > 0)
             {
-                return 1;
+                //跌破均线 考虑走人
+                if (V_Cache.V_KLineData[0].V_ClosePrice <= MAPrice)
+                {
+                    return 1;
+                }
+            }
+            else if (orderDir < 0)
+            {
+                //涨破均线 考虑走人
+                if (V_Cache.V_KLineData[0].V_ClosePrice >= MAPrice)
+                {
+                    return 1;
+                }
             }
         }
 
